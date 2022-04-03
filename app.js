23dd7311b79c8a4d6296a9c565a1884dfe88a982
Arseniy <arsenij.rabets@gmail.com>
2022-04-03 16:14:10 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const Registration = {
+ 
+}
+
+function registrationInputs(){
+  Registration.login = document.getElementById("login").value;
+  Registration.pass = document.getElementById("pass").value;
+  Registration.age = document.getElementById("age").value;
+  Registration.adress = document.getElementById("adress").value;
+  Registration.nickname = document.getElementById("nick").value;
+  
+ 
+
+  return console.log(Registration)
+}
